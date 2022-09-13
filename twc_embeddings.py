f6958e89d1433bea4fc84d07de71ae929f01ac34
taskswithcode <farminaround@gmail.com>
2022-09-13 02:04:43 +0000

    Change

--- twc_embeddings.py
+++ twc_embeddings.py
@@ -1,8 +1,14 @@
-from simcse import SimCSE
 import pdb
 from scipy.spatial.distance import cosine
 import argparse
 import json
+import os,sys
+
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+print(CURR_DIR)
+sys.path.append(CURR_DIR)
+
+from simcse import SimCSE
 
 
 def read_text(input_file):
@@ -15,9 +21,13 @@ class SimCSEModel:
     def __init__(self):
         self.model = None
         self.tokenizer = None
+        self.debug = False
+        print("In SimCSE constructor")
 
-    def init_model(self):
-        self.model = SimCSE("princeton-nlp/sup-simcse-bert-base-uncased")
+    def init_model(self,model_name = None):
+        if (model_name == None):
+            model_name = "princeton-nlp/sup-simcse-roberta-large"
+        self.model = SimCSE(model_name)
 
     def compute_embeddings(self,input_data,is_file):
         texts = read_text(input_data) if is_file == True else input_data
@@ -34,11 +44,12 @@ class SimCSEModel:
 
         print("Input sentence:",texts[0])
         sorted_dict = dict(sorted(cosine_dict.items(), key=lambda item: item[1],reverse = True))
-        for key in sorted_dict:
-            print("Cosine similarity with  \"%s\" is: %.3f" % (key, sorted_dict[key]))
+        if (self.debug):
+            for key in sorted_dict:
+                print("Cosine similarity with  \"%s\" is: %.3f" % (key, sorted_dict[key]))
         if (output_file is not None):
             with open(output_file,"w") as fp:
-                fp.write(json.dumps(sorted_dict))
+                fp.write(json.dumps(sorted_dict,indent=0))
         return sorted_dict
 
 
@@ -46,8 +57,9 @@ if __name__ == '__main__':
         parser = argparse.ArgumentParser(description='SGPT model for sentence embeddings ',formatter_class=argparse.ArgumentDefaultsHelpFormatter)
         parser.add_argument('-input', action="store", dest="input",required=True,help="Input file with sentences")
         parser.add_argument('-output', action="store", dest="output",default="output.txt",help="Output file with results")
+        parser.add_argument('-model', action="store", dest="model",default="princeton-nlp/sup-simcse-bert-base-uncased",help="model name")
         results = parser.parse_args()
         obj = SimCSEModel()
-        obj.init_model()
+        obj.init_model(results.model)
         texts, embeddings = obj.compute_embeddings(results.input,is_file = True)
         results = obj.output_results(results.output,texts,embeddings)
